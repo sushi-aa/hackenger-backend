452dd5dd6627f6958b34085b935ae6eb2b750152
sushi-aa <57570689+sushi-aa@users.noreply.github.com>
2019-11-10 15:09:29 -0800

    Add files via upload

--- login_system_backend.py
+++ login_system_backend.py
@@ -1,28 +1,62 @@
-from flask import flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
-import os
+from flask import Flask, render_template, url_for, request, session, redirect
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+import bcrypt
 
+client = MongoClient('localhost', 27017)
+db = client.some_database
+collection = client.db['database']
 app = Flask(__name__)
 
+the_dictionary = request.data
+username = the_dictionary.keys()
+password = request.data.values()
+
+#users_and_pass = dict()
+#need variable to store username, get it from database
+#need variable to store password, get that from databses
+
+app.config['MONGO_DBNAME'] = 'mongodb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017' #need URL
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def homepage():
-	if not session.get('logged_in'):
-		return render_template(' ')
-	else:
-		return "hello" <a hfref="/logout">Logout</a>""
+def index():
+  if request.data.username in session: #need username in quotes
+    return ('You\'re logged in!', list_of_contacts)
+  return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
-def admin_login():
-	if request.form['password'] == ' ' and request.form['username'] == ' ':
-		session['logged_in'] = True
-	else:
-		flash("incorrect password!")
+def login():
+  users = mongo.db.users
+  for x in users.keys():
+        #list_of_users.append(x)
 
-	return home()
+    login_user = users.find_one({'name' : request.form[username]}) #need variable
 
-def logout():
-	session['logged_in'] = False
-	return home()
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user[username].encode('utf-8')) == login_user[''].encode('utf-8'):
+            session[username] = request.form[username]
+            return redirect(url_for('index'))
 
+    return 'Invalid username/password'
+    
 
+@app.route('/register', methods=['POST', 'GET'])
+def register_user():
+   if request.method == 'POST':
+       users = mongo.db.users
+       existing_user = users.find_one({'name' : request.form[username]})
 
+       if existing_user is None:
+           hashpass = bcrypt.hashpw(request.form[username].encode('utf-8'), bcrypt.gensalt())
+           users.insert({'name' : request.form[username], '' : hashpass})
+           session[username] = request.form[username]
+           return redirect(url_for('index'))
+
+       return render_template('register.html')
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(debug=True)
